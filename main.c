03f5fcfdc38c8d4b5a31afe18b19d3e4a9fed917
QwazyWabbit <gandalf.2004@gmail.com>
2023-07-31 10:08:54 -0700

    Renamed the function and avoid calling strlen.

--- main.c
+++ main.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
-size_t StrRemoveTrailingNewlines(char* str);
+size_t strrrnl(char* str);
 
 /* Remove all trailing newlines from a C string.
 Input: pointer to the string to be altered in place
 Return: length of resulting string */
-size_t StrRemoveTrailingNewlines(char* str)
+size_t strrrnl(char* str)
 {
-    char* p = str + strlen(str) - 1;  // grab the tail
+    char* p = str + strlen(str) - 1;
     while (*p == '\n')
     {
         *p = '\0';
         p--;
     }
-    return strlen(str);
+    return (p - str + 1);
 }
 
 int main(void)
 {
+    size_t len;
     char string0[] = "This is a string with 0 newlines.";
     char string1[] = "This is a string with 1 newline.\n";
     char string2[] = "This is a string with 2 newlines.\n\n";
     char string3[] = "This is a string with 3 newlines.\n\n\n";
-    char string4[] = "This is a string\nwith 1 embedded newline.\n\n";
+    char string4[] = "This is a string\nwith 1 embedded newline and two trailing.\n\n";
 
     puts("The initial strings when not trimmed:");
     printf("%s\n", string0);
@@ -31,18 +32,19 @@ int main(void)
     printf("%s\n", string2);
     printf("%s\n", string3);
     printf("%s\n", string4);
- 
-    StrRemoveTrailingNewlines(string0);
-    StrRemoveTrailingNewlines(string1);
-    StrRemoveTrailingNewlines(string2);
-    StrRemoveTrailingNewlines(string3);
-    StrRemoveTrailingNewlines(string4);
 
-    puts("The following should have no extra linefeeds:");
+    len = strrrnl(string0);
+    len = strrrnl(string1);
+    len = strrrnl(string2);
+    len = strrrnl(string3);
+    len = strrrnl(string4);
+
+    puts("The modified strings after trimming:");
     printf("%s\n", string0);
     printf("%s\n", string1);
     printf("%s\n", string2);
     printf("%s\n", string3);
     printf("%s\n", string4);
     puts("Test ends.");
+    return 0;
 }
